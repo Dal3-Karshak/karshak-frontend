ca86fb16daf6d2f9e8ac401701221293b7c8c483
FerasSadTawfiqEzz-aldeen <Ferasezaldeen@gmail.com>
2021-06-25 20:57:57 +0300

    starting

---
+++ karshak/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          
+        </Router>
+      </div>
+    )
+  }
+}
+
+export default App
+
